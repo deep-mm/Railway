913d442ec80097dcbdbf52b2d9a39d49f29bc3ec
Bhavik <bhavik.parmar@somaiya.edu>
2018-06-22 03:26:23 +0530

    Train Entity,Utilities,Listener done

---
+++ Sample Projects/BackRail/app/src/main/java/com/kjsce/train/cia/Listener/GetBogeyListListener.java
@@ -0,0 +1,7 @@
+package com.kjsce.train.cia.Listener;
+
+import com.kjsce.train.cia.Entities.TrainEntity;
+
+public interface GetBogeyListListener {
+    void onCompleteTask(TrainEntity trainEntity);
+}
