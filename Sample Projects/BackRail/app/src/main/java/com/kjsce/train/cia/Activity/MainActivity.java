dc702df6e68ce8a5cb6e0891fa8b3bdeaa1376fb
Bhavik <bhavik.parmar@somaiya.edu>
2018-06-21 23:40:41 +0530

    activitiess

---
+++ Sample Projects/BackRail/app/src/main/java/com/kjsce/train/cia/Activity/MainActivity.java
@@ -0,0 +1,37 @@
+package com.kjsce.train.cia.Activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.kjsce.train.cia.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Intent intent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void toUser(View view){
+        intent = new Intent(this,UserActivity.class);
+        startActivity(intent);
+    }
+
+    public void toTrain(View view){
+        intent = new Intent(this,TrainActivity.class);
+        startActivity(intent);
+    }
+
+    public void toBogey(View view){
+        intent = new Intent(this,BogeyActivity.class);
+        startActivity(intent);
+    }
+
+}
