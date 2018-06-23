20b381d59b87d65006f1bd1f158138c7878f835f
Bhavik <bhavik.parmar@somaiya.edu>
2018-06-23 15:25:16 +0530

    Train Ultity and Activity Changed

--- Sample Projects/BackRail/app/src/main/java/com/kjsce/train/cia/Activity/TrainActivity.java
+++ Sample Projects/BackRail/app/src/main/java/com/kjsce/train/cia/Activity/TrainActivity.java
@@ -11,12 +11,24 @@ import com.kjsce.train.cia.Listener.AddTrainListener;
 import com.kjsce.train.cia.Listener.DeleteBogeyListener;
 import com.kjsce.train.cia.Listener.GetBogeyListListener;
 import com.kjsce.train.cia.Listener.GetTrainListListener;
+import com.kjsce.train.cia.Listener.OnBogeyListChangeListener;
+import com.kjsce.train.cia.Listener.OnTrainListChangeListener;
 import com.kjsce.train.cia.R;
+import com.kjsce.train.cia.Utilities.TrainListUtility;
 import com.kjsce.train.cia.Utilities.TrainUtility;
 
 import java.util.List;
 
 public class TrainActivity extends AppCompatActivity {
+    private int i=0;
+
+    private TrainUtility trainUtility = new TrainUtility("8511100", new OnBogeyListChangeListener() {
+        @Override
+        public void onDataChanged(TrainEntity newTrainEntity) {
+            Toast.makeText(getApplicationContext(), newTrainEntity.toString(), Toast.LENGTH_LONG).show();
+        }
+    });
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,54 +37,51 @@ public class TrainActivity extends AppCompatActivity {
     }
 
     public void addTrainB(View view){
-        TrainUtility trainUtility = new TrainUtility();
+       /* TrainUtility trainUtility = new TrainUtility();
         trainUtility.addTrain("8511100", new AddTrainListener() {
             @Override
             public void onCompleteTask(String result) {
                 Toast.makeText(getApplicationContext(),result, Toast.LENGTH_LONG).show();
             }
-        });
+        });*/
     }
 
     public void addBogeyB(View view){
-        TrainUtility trainUtility = new TrainUtility();
-        trainUtility.addBogey("805", "8511100", new AddBogeyListener() {
-            @Override
-            public void onCompleteTask(String result) {
-                Toast.makeText(getApplicationContext(),result, Toast.LENGTH_LONG).show();
-            }
-        });
+
+        trainUtility.addBogey("800");
     }
 
     public void getBogeyListB(View view){
-        TrainUtility trainUtility = new TrainUtility();
+        /*TrainUtility trainUtility = new TrainUtility();
         trainUtility.getBogeyList("8511100", new GetBogeyListListener() {
             @Override
             public void onCompleteTask(TrainEntity trainEntity) {
                 Toast.makeText(getApplicationContext(), trainEntity.toString(), Toast.LENGTH_LONG).show();
             }
-        });
+        });*/
     }
 
     public void deleteBogeyB(View view){
-        TrainUtility trainUtility = new TrainUtility();
-        trainUtility.deleteBogey("804", "8511100", new DeleteBogeyListener() {
-            @Override
-            public void onCompleteTask(String result) {
-                Toast.makeText(getApplicationContext(),result, Toast.LENGTH_LONG).show();
-            }
-        });
+        trainUtility.deleteBogey("8002");
+
 
     }
 
     public void getTrainListB(View view){
+         TrainListUtility trainListUtility = new TrainListUtility(new OnTrainListChangeListener() {
+            @Override
+            public void OnDataChenged(List<String> newTrainList) {
+                Toast.makeText(getApplicationContext(),"Train List   "+newTrainList.toString(),Toast.LENGTH_LONG).show();
+            }
+        });
+        /*
         TrainUtility trainUtility = new TrainUtility();
         trainUtility.getTrainList(new GetTrainListListener() {
             @Override
             public void onCompleteTask(List<String> trainList) {
                 Toast.makeText(getApplicationContext(),trainList.toString(),Toast.LENGTH_LONG).show();
             }
-        });
+        });*/
 
     }
 
